a97084fbd32739250de78da156b773260b8118ab
Brandon Louw <brandonmlouw@gmail.com>
2025-10-19 11:01:01 -0600

    Add logger middleware

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-window.onload = () => {
-    //console.log('This is your first program executing!');
-}
+const express = require('express');
+const logger = require('./loggerMiddleware');
+
+const app = express();
+
+app.use(logger);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ loggerMiddleware.js
@@ -0,0 +1,7 @@
+// loggerMiddleware.js
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+module.exports = logger;
